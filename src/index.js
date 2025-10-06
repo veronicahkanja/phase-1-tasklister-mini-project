55b6b4a78fccaa37735792b2252df88359e518bd
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-10-06 16:07:05 +0300

    complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // first select the form
+  const form = document.getElementById("create-task-form");
+
+  // second add an event listener for when a form is submitted
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    // third Get the value from the input box
+    const input = document.getElementById("new-task-description");
+    const task = input.value.trim(); 
+
+    // fourth only add if input is not empty
+    if (task !== "") {
+      const li = document.createElement("li");
+      li.textContent = task;
+
+      // fifth append the new task to the task list
+      const taskList = document.getElementById("tasks");
+      taskList.appendChild(li);
+
+      // sixth reset input box
+      input.value = "";
+    }
+  });
+ 
 });
